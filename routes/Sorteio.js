054135504ac25c4e2d3cd171963bf8da5610bba3
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2023-06-21 00:40:56 -0300

    Update Sorteio.js

--- routes/Sorteio.js
+++ routes/Sorteio.js
@@ -7,7 +7,7 @@ router.get('/', async (req,res)=>{
     const times = await Team.find()
     const lista_times = []
     times.forEach(element => {
-        lista_times.push(element.name)
+        lista_times.push([element.name,element.img])
     });
     //Montando partidas 
     let partidas = []
@@ -17,15 +17,15 @@ router.get('/', async (req,res)=>{
         while(partidas.length != 4){
             let x = Math.floor(Math.random() * p_copia.length)
             let y = Math.floor(Math.random() * p_copia.length)
-            var teamA = p_copia[x]
-            var teamB = p_copia[y]
+            var teamA = p_copia[x][0]
+            var teamB = p_copia[y][0]
             if(teamA!=teamB && (teamA!=0 && teamB!=0)){
                 p_copia[x]=0
                 p_copia[y]=0
                 partidas.push(teamA+" VS "+teamB)
             }      
         }
-        res.json(partidas)
+        res.json([partidas,lista_times])
     }
     
 })
